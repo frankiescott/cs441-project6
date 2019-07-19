793ba844e043082f8a4a3b4715fbd6fc049e97de
Frankie <email@frankiescott.net>
2019-07-19 19:40:56 -0400

    created splash screen class

--- core/src/edu/binghamton/project6/MyGame.java
+++ core/src/edu/binghamton/project6/MyGame.java
@@ -1,6 +1,7 @@
 package edu.binghamton.project6;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
