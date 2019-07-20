9964e5751160c970a7586580c377b999ada95bee
Frankie <email@frankiescott.net>
2019-07-20 15:00:52 -0400

    created menu screen class

---
+++ core/src/edu/binghamton/project6/MenuScreen.java
@@ -0,0 +1,53 @@
+package edu.binghamton.project6;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuScreen implements Screen {
+    private SpriteBatch batch;
+
+    public MenuScreen() {
+        super();
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.end();
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
