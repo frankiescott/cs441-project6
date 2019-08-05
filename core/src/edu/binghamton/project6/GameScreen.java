0a3351db86aa8d25a3da8fe67592e7b4c063ab41
Frankie <email@frankiescott.net>
2019-08-05 12:30:55 -0400

    game screen spawns random rectangles every second

--- core/src/edu/binghamton/project6/GameScreen.java
+++ core/src/edu/binghamton/project6/GameScreen.java
@@ -5,15 +5,22 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class GameScreen implements Screen {
     private final MyGame app;
     private SpriteBatch batch;
-    private Texture splashImg;
+    private Texture splashImg, objectImage;
 
     private float w, h;
 
     private int score;
+
+    private Array<Rectangle> objects;
+    private long spawnTime;
 
     public void gameEnd(int score) {
         app.setScreen(new ScoreScreen(app, score));
@@ -25,8 +32,22 @@ public class GameScreen implements Screen {
 
         batch = new SpriteBatch();
         splashImg = new Texture("bg.png");
+        objectImage = new Texture("block.png");
 
         score = 0;
+
+        objects = new Array<Rectangle>();
+        spawnObject();
+    }
+
+    private void spawnObject() {
+        Rectangle object = new Rectangle();
+        object.x = MathUtils.random(0, app.width - 65);
+        object.y = MathUtils.random(0, app.height - 65);
+        object.width = 65;
+        object.height = 65;
+        spawnTime = TimeUtils.millis();
+        objects.add(object);
     }
 
     @Override
@@ -36,9 +57,15 @@ public class GameScreen implements Screen {
 
         batch.begin();
         batch.draw(splashImg, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        for(Rectangle object: objects) {
+            batch.draw(objectImage, object.x, object.y);
+        }
+        if (TimeUtils.millis() - spawnTime > 1000) {
+            spawnObject();
+        }
         batch.end();
 
-        gameEnd(5000);
+        //gameEnd(5000);
     }
 
     @Override
